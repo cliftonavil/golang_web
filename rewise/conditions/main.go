cfb09bd5a38fa21c09e7897895a7bcb45ec173f4
clifton <cliftonavil@gmail.com>
2019-05-03 22:37:27 +0530

    final

--- rewise/conditions/main.go
+++ rewise/conditions/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// this function will be called
+//this function will be called
 func gettotal(a, b float64) float64 {
 	return a + b
 }
@@ -17,33 +18,33 @@ func second() {
 }
 
 func main() {
-	// //if condition
-	// a := 10
-	// b := 20
+	//if condition
+	a := 10
+	b := 20
 
-	// if a > b {
-	// 	fmt.Println(a, "is greater then", b)
-	// } else {
-	// 	fmt.Println(b, "is greater then", a)
-	// }
-	// // passing parameters to function
-	// fmt.Println("function returned :", gettotal(10, 90))
-	// //switch case
-	// today := time.Now()
-	// switch today.Day() {
-	// case 5:
-	// 	fmt.Println("Today is 5th. Clean your house.")
-	// case 10:
-	// 	fmt.Println("Today is 10th. Buy some wine.")
-	// case 15:
-	// 	fmt.Println("Today is 15th. Visit a doctor.")
-	// case 25:
-	// 	fmt.Println("Today is 25th. Buy some food.")
-	// case 31:
-	// 	fmt.Println("Party tonight.")
-	// default:
-	// 	fmt.Println("No information available for that day.")
-	// }
+	if a > b {
+		fmt.Println(a, "is greater then", b)
+	} else {
+		fmt.Println(b, "is greater then", a)
+	}
+	// passing parameters to function
+	fmt.Println("function returned :", gettotal(10, 90))
+	//switch case
+	today := time.Now()
+	switch today.Day() {
+	case 5:
+		fmt.Println("Today is 5th. Clean your house.")
+	case 10:
+		fmt.Println("Today is 10th. Buy some wine.")
+	case 15:
+		fmt.Println("Today is 15th. Visit a doctor.")
+	case 25:
+		fmt.Println("Today is 25th. Buy some food.")
+	case 31:
+		fmt.Println("Party tonight.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
 
 	defer second()
 	first()
