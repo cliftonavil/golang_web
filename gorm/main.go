abe832c53513d5e718d895c7739a5d15a21fbdb1
clifton <cliftonavil@gmail.com>
2019-09-11 21:17:07 +0530

    final

--- gorm/main.go
+++ gorm/main.go
@@ -1,62 +1,62 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"time"
+// import (
+// 	"fmt"
+// 	"time"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
-)
+// 	"github.com/jinzhu/gorm"
+// 	_ "github.com/lib/pq"
+// )
 
-func main() {
-	db, err := gorm.Open("postgres", "user=postgres password=clifton dbname=gorm sslmode=disable")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
+// func main() {
+// 	db, err := gorm.Open("postgres", "user=postgres password=clifton dbname=gorm sslmode=disable")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	defer db.Close()
 
-	fmt.Println("Connected to Database!!")
+// 	fmt.Println("Connected to Database!!")
 
-	// err = db.DB().Ping()  //Checking for Error
-	// fmt.Println(err)
+// 	// err = db.DB().Ping()  //Checking for Error
+// 	// fmt.Println(err)
 
-	// db.SingularTable(false) // Table Name with 's
+// 	// db.SingularTable(false) // Table Name with 's
 
-	db.Debug().DropTableIfExists(&Owner{}, &Book{}, &Author{})
-	db.Debug().CreateTable(&Owner{}, &Book{}, &Author{})
+// 	db.Debug().DropTableIfExists(&Owner{}, &Book{}, &Author{})
+// 	db.Debug().CreateTable(&Owner{}, &Book{}, &Author{})
 
-	owner := Owner{
-		FirstName: "CLifton",
-		LastName:  "Avil",
-	}
-	db.Debug().Create(&owner)
-	owner.LastName = "Dsouza" // Update useing current object
-	db.Debug().Save(&owner)   // save
+// 	owner := Owner{
+// 		FirstName: "CLifton",
+// 		LastName:  "Avil",
+// 	}
+// 	db.Debug().Create(&owner)
+// 	owner.LastName = "Dsouza" // Update useing current object
+// 	db.Debug().Save(&owner)   // save
 
-	// db.Debug().Delete(&owner) // Delete Record useing current  object
+// 	// db.Debug().Delete(&owner) // Delete Record useing current  object
 
-	var o Owner
-	db.Debug().Find(&o)
-	fmt.Println(o.FirstName, o.LastName)
-}
+// 	var o Owner
+// 	db.Debug().Find(&o)
+// 	fmt.Println(o.FirstName, o.LastName)
+// }
 
-type Owner struct {
-	gorm.Model
-	FirstName string
-	LastName  string
-	Books     []Book
-}
+// type Owner struct {
+// 	gorm.Model
+// 	FirstName string
+// 	LastName  string
+// 	Books     []Book
+// }
 
-type Book struct {
-	gorm.Model
-	Name        string
-	PublishDate time.Time
-	OwnerID     uint     `sql:"index"`
-	Authors     []Author `gorm:"many2many:books_authors"`
-}
+// type Book struct {
+// 	gorm.Model
+// 	Name        string
+// 	PublishDate time.Time
+// 	OwnerID     uint     `sql:"index"`
+// 	Authors     []Author `gorm:"many2many:books_authors"`
+// }
 
-type Author struct {
-	gorm.Model
-	FirstName string
-	LastName  string
-}
+// type Author struct {
+// 	gorm.Model
+// 	FirstName string
+// 	LastName  string
+// }
