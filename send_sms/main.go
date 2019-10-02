13e4be99802d1f8e3bdc8a1357dbd3ce0080f467
clifton <cliftonavil@gmail.com>
2019-10-02 21:23:09 +0530

    final

---
+++ send_sms/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+// getRandNum returns a random number of size four
+func OtpContent() string {
+	nBig, e := rand.Int(rand.Reader, big.NewInt(8999))
+	if e != nil {
+		fmt.Println("xxx", e)
+	}
+	return "We are currently processing ticket number " + strconv.FormatInt(nBig.Int64()+1000, 10)
+}
+
+func main() {
+	fmt.Println(OtpContent())
+}
